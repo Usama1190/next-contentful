4b61f8d27775763a285cd24b214139e885e59d9b
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-28 12:16:39 -0700

    Keep learning on Aug 29, 24

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// import { space } from 'postcss/lib/list';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    env: {
+        CONTENTFUL_SPACE_ID: 'o73qsurkfns2',
+        CONTENTFUL_ENVIRONMENT: 'master',
+    }
+};
 
 export default nextConfig;
